834a675c444c58d81698733b5afa5c6910b3c55a
nik gaffney <nik@fo.am>
2021-05-10 21:33:00 +0200

    KC_HYPR

---
+++ rules.mk
@@ -0,0 +1,18 @@
+ # START-MUGUR-REGION
+
+ FORCE_NKRO       = yes
+     LEADER_ENABLE    = no
+     RGBLIGHT_ENABLE  = yes
+     TAP_DANCE_ENABLE = no
+     COMBO_ENABLE     = no 
+
+# END-MUGUR-REGION  BOOTLOADER = atmel-dfu  # Elite-C
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes # WS2812 # per-key RGB and underglow
+
+UNICODE_ENABLE = yes
+
+OLED_DRIVER_ENABLE = yes
+MOUSEKEY_ENABLE = no  # use mouse keys or not
+CONSOLE_ENABLE = no  # debug info

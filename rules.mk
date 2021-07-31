e2c9f4d36435b0f3f04e4aed5e27f96411ef04ce
nik gaffney <nik@fo.am>
2021-07-31 12:41:28 +0200

    MOD_RGUI

--- rules.mk
+++ rules.mk
@@ -1,18 +1,27 @@
- # START-MUGUR-REGION
+
+BOOTLOADER = dfu  # Elite-C
+
+# START-MUGUR-REGION
 
  FORCE_NKRO       = yes
-     LEADER_ENABLE    = no
-     RGBLIGHT_ENABLE  = yes
-     TAP_DANCE_ENABLE = no
-     COMBO_ENABLE     = no 
+ LEADER_ENABLE    = no
+ RGBLIGHT_ENABLE  = no
+ TAP_DANCE_ENABLE = no
+ COMBO_ENABLE     = no
 
-# END-MUGUR-REGION  BOOTLOADER = atmel-dfu  # Elite-C
+# END-MUGUR-REGION
+
+NKRO_ENABLE = no # six simultaneous keys should be enough
+
 
 RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes # WS2812 # per-key RGB and underglow
+RGB_MATRIX_ENABLE = no # yes # WS2812 # per-key RGB and underglow
+
+VIA_ENABLE = yes
 
 UNICODE_ENABLE = yes
 
 OLED_DRIVER_ENABLE = yes
 MOUSEKEY_ENABLE = no  # use mouse keys or not
+
 CONSOLE_ENABLE = no  # debug info

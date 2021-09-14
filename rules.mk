63b89e2747aa59dce267a1a99ed19569d4e8e5e4
nik gaffney <nik@fo.am>
2021-09-14 19:07:43 +0200

    KC_EXCLAIM

--- rules.mk
+++ rules.mk
@@ -1,27 +1,22 @@
-
-BOOTLOADER = dfu  # Elite-C
-
 # START-MUGUR-REGION
 
- FORCE_NKRO       = yes
- LEADER_ENABLE    = no
- RGBLIGHT_ENABLE  = no
- TAP_DANCE_ENABLE = no
- COMBO_ENABLE     = no
+FORCE_NKRO       = yes
+LEADER_ENABLE    = no
+RGBLIGHT_ENABLE  = yes
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE     = no
 
 # END-MUGUR-REGION
 
-NKRO_ENABLE = no # six simultaneous keys should be enough
+BOOTLOADER         = dfu # Elite-C
 
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes # WS2812 # per-key RGB and underglow
 
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = no # yes # WS2812 # per-key RGB and underglow
-
-VIA_ENABLE = yes
-
-UNICODE_ENABLE = yes
+UNICODE_ENABLE     = yes
 
 OLED_DRIVER_ENABLE = yes
-MOUSEKEY_ENABLE = no  # use mouse keys or not
+MOUSEKEY_ENABLE    = no  # use mouse keys or not
+CONSOLE_ENABLE     = no  # debug info
 
-CONSOLE_ENABLE = no  # debug info
+VIA_ENABLE         = no  # to enable configuraiton with VIA
